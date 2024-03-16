8462f508aca2134623d28c249b24906ba1dcb917
japhethh <boknoy32130@gmail.com>
2024-03-16 23:10:21 +0800

    Hero paren

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,8 @@ export default {
       '2xl': ['24px', '29.26px'],
       '3xl': ['28px', '50px'],
       '4xl': ['48px', '58px'],
-      '8xl': ['96px', '106px']
+      '8xl': ['96px', '106px'],
+      '9xl': ['100px','100px']
     },
     extend: {
       fontFamily: {
