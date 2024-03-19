47da1f2b73cb21d6d96cf41d08f6ba6e98f59bd2
japhethh <boknoy32130@gmail.com>
2024-03-19 12:33:50 +0800

    SuperQualilty finish❤️

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
-
-const Button = ({label, iconUrl}) => {
+const Button = ({ label, iconUrl }) => {
   return (
-    <button className=" flex justify-center items-center gap-2 px-7 py-4 border font-monserrat text-lg leading-none bg-coral-red rounded-full text-white" >{label}
-    <img className="ml-2 rounded-full  h-5 w-5" src={iconUrl} alt={label} />
+    <button className=" flex justify-center items-center gap-2 px-7 py-4 border font-monserrat text-lg leading-none bg-coral-red rounded-full text-white">
+      {label}
+      { iconUrl &&  <img className="ml-2 rounded-full  h-5 w-5" src={iconUrl} alt={label} />}
     </button>
-    )
-}
+  );
+};
 
-export default Button
+export default Button;
