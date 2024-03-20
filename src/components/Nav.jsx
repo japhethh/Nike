8fc92e56fe82d9e2ada2ec55a7ac28fa4d6757b0
japhethh <boknoy32130@gmail.com>
2024-03-20 12:47:45 +0800

    Finish ✅

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,24 +1,59 @@
+import React, { useState } from "react";
 import { headerLogo } from "../assets/images";
 import { hamburger } from "../assets/icons";
 import { navLinks } from "../constants";
 
 const Nav = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <header className="padding-x py-8 absolute z-10 w-full">
       <nav className="flex justify-between items-center max-container">
         <a href="/">
-          <img src={headerLogo} alt="logo" width={130} height={29} /> 
+          <img src={headerLogo} alt="logo" width={130} height={29} />
         </a>
-        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden ">
+        <ul className="hidden md:flex justify-center items-center gap-16">
           {navLinks.map((item) => (
             <li key={item.label}>
-              <a href={item.href} className="font-monserrat leading-normal text-lg text-slate-gray">{item.label}</a>
+              <a
+                href={item.href}
+                className="font-monserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
             </li>
           ))}
         </ul>
-        <div>
-          <img src={hamburger} alt="Hamburger" width={25} height={25} className="hidden max-lg:block"/>
+
+        <div className="md:hidden" onClick={toggleMenu}>
+          <img src={hamburger} alt="Hamburger" width={25} height={25} />
         </div>
+        {isMenuOpen && (
+          <div className="md:hidden absolute w-full top-16 left-0 bg-white py-4 px-8 shadow-lg">
+            <div className="flex justify-content items-center flex-col font-semibold leading-normal text-4xl bg-white z-30">
+              <ul>
+                {navLinks.map((item) => (
+                  <li key={item.label}>
+                    <a
+                      href={item.href}
+                      className="font-monserrat leading-normal text-lg text-slate-gray"
+                    >
+                      {item.label}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        )}
       </nav>
     </header>
   );
